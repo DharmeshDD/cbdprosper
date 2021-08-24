3f9505091c042342ac775c0bd639d362d9ff7502
DharmeshDD <dharmesh@techyscouts.com>
2021-08-24 12:39:56 +0530

    Home page

--- assets/custom.js
+++ assets/custom.js
@@ -14,7 +14,58 @@ $('#announcement-bar').owlCarousel({
             items:1
         }
     }   
-})
+});
+
+$('#home-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }   
+});
+
+$('#shop-bestseller').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }   
+});
+
+$('#customer-review').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }   
+});
 
 $(".announcement-close-icon").click(function(){
    $(".announcement").addClass('d-none');
